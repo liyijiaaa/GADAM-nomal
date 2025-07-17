a57a389b01767d7f2b93de6c2d35352aad8477ea
liyijiaaa <2979038380@qq.com>
2025-07-17 20:55:41 +0800

    GADAM

--- model.py
+++ model.py
@@ -185,10 +185,10 @@ class GlobalModel(nn.Module):
         return center_loss, scores
 
 
-    def forward(self, feats, epoch, ada_neighbor_nodes):
-        # h, mean_h = self.encoder(feats)
-        h, _ = self.encoder(feats)
-        mean_h = torch.mean(h[ada_neighbor_nodes], dim=1)  # 转换为 [n_nodes, feat_dim]
+    def forward(self, feats, epoch):
+        h, mean_h = self.encoder(feats)
+        #h, _ = self.encoder(feats)
+        #mean_h = torch.mean(h[ada_neighbor_nodes], dim=1)  # 转换为 [n_nodes, feat_dim]
 
         post_attn = self.post_attention(h, mean_h)
         beta = math.pow(self.beta, epoch)
